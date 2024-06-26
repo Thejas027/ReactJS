0a7a4c06729fe9169c775a877737e040e1dbbf35
Thejas <teju270934@gmail.com>
2024-06-27 00:20:16 +0530

    learnt how props can made use in JSx syntax and updated the syting of webpage

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -68,10 +68,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu </h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato,mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 1}</span>
+      </div>
+    </div>
   );
 }
 
@@ -84,16 +107,6 @@ function Footer() {
 
   return <footer className="footer">{hour}. We're currently open!</footer>;
   // return React.createElement("footer",null,"We're currently open!")
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3  > Pizza Spinaci </h3>
-      <p> Tomato, mozarella, spinach, and ricotta cheese </p>
-    </div>
-  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
