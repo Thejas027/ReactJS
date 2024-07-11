a7602c07b38effd765c83aa02580c737187a26bd
Thejas <teju270934@gmail.com>
2024-07-11 23:51:26 +0530

    started working on the another project where it conculdes all the stuff that has been learnt till here

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -21,34 +21,92 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <div>
-      <Members />
-      <NewMember />
-      <BillSplit />
+    <div className="app">
+      <idv className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </idv>
+      <FormSplitBill />
     </div>
   );
 }
 
-function Members() {
+function FriendsList() {
+  const friends = initialFriends;
+
   return (
-    <div className="form">
-      <h2>Thejas</h2>
-    </div>
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
   );
 }
 
-function NewMember() {
+function Friend({ friend }) {
   return (
-    <div>
-      <h4>New Name</h4>
-    </div>
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      34
+      <Button>Select</Button>
+    </li>
   );
 }
 
-function BillSplit() {
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
   return (
-    <div>
-      <h1>Split Bill </h1>
-    </div>
+    <form className="form-add-friend">
+      <label>👯‍♀️ Frnd Name</label>
+      <input type="text" />
+
+      <label>🌃 Image URL</label>
+      <input type="text" disabled />
+
+      <Button>Add </Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️Your expense</label>
+      <input type="text" />
+
+      <label>👯‍♀️X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
