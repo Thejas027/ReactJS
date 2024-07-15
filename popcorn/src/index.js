5b0741f3c55dcbc33913884294c1398e9243fdd1
Thejas <teju270934@gmail.com>
2024-07-15 18:51:50 +0530

    updated the star component file where the whole function has been given a outside acces to use it accrodingly and made it generic

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./components/StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      // message={["Terrible", "Bad", "okay", "Good", "Amazing"]}
+      message={[]}
+      color="#fcc419"
+      size={48}
+      className=""
+      defaultRating={0}
+    />
+    <Test />
   </React.StrictMode>
 );
