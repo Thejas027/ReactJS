038f54ab98d8d6e4a517b85eedf1004c061afb64
Thejas <teju270934@gmail.com>
2024-07-18 23:07:16 +0530

    updated the tab content rendering part where batching of states is made and functions to update the 3 likes and undo 2 seconds later buttons were updated

--- Cls-Challenges/behindscences/src/App.js
+++ Cls-Challenges/behindscences/src/App.js
@@ -66,7 +66,35 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);  // this really wont updates the likes three times, since for each function call the state gets reset,
+
+    // this could be done but still
+    //setLikes(likes + 3);
+
+    // instead use call back functions
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1); // call back function updates the with the current states
+
+    // // or this could be done a calling a above handleInc three times
+    // handleInc();
+    // handleInc();
+    // handleInc();
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
   }
 
   return (
@@ -82,13 +110,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
