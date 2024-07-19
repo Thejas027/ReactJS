00b3a40745fcde6fc81c3c44c317a02faa6a9e6e
Thejas <teju270934@gmail.com>
2024-07-19 22:14:14 +0530

    back in track, working on popcorn website, useEffect is used to fetch a movies list form the api

--- popcorn/src/App-copy.js
+++ popcorn/src/App-copy.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,18 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "2363428c";
-
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  useEffect(function () {
-    fetch(`https://www.omdbapi.com?apikey=${KEY}&s=interstellar`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
-  }, []);
-
   return (
     <>
       <NavBar>
--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./components/StarRating";
 
@@ -19,8 +19,8 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       // message={["Terrible", "Bad", "okay", "Good", "Amazing"]}
       message={[]}
@@ -28,7 +28,6 @@ root.render(
       size={48}
       className=""
       defaultRating={0}
-    />
-    {/* <Test /> */}
+    /> */}
   </React.StrictMode>
 );
