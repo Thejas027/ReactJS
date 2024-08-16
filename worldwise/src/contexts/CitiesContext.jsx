d60dfb9420f70b9bdd0e7a864d8328da61008c42
Thejas <teju270934@gmail.com>
2024-08-16 23:37:02 +0530

    added another component file called backbutton component and updated the functionlaity part of having which city it has been wnent basically and highlight part was made to add it

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useContext } from "react";
 import { useEffect, useState, createContext } from "react";
 
 const BASE_URL = "http://localhost:9000";
@@ -7,6 +8,9 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  // global state declaration not a local state (since its used for multiple components)
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -24,11 +28,26 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was a error in loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
@@ -36,4 +55,12 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("Cites context was used outside the citiesProvider");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
