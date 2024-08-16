7e944de476780078d847fcaa1d0222ac418c34db
Thejas <teju270934@gmail.com>
2024-08-16 17:47:11 +0530

    modified the app.js file with adding programatic navigation to the project with an button to it

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,13 +1,15 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
 
+function Map() {
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       Map
       <h1>
         Position : {lat} and {lng}
