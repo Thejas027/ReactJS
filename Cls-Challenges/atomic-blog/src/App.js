d117ee5aa09c0f21747e0787d809ab31ff2b5083
Thejas <teju270934@gmail.com>
2024-08-19 22:43:18 +0530

    updated the app.js file where rerender time was large made it some sort of faster loding by using memo and callback function to it and in context file also it has been applied

--- Cls-Challenges/atomic-blog/src/App.js
+++ Cls-Challenges/atomic-blog/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { usePosts, PostProvider } from "./PostContext";
 
@@ -74,14 +74,14 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
   return (
     <main>
       <FormAddPost />
       <Posts />
     </main>
   );
-}
+});
 
 function Posts() {
   return (
