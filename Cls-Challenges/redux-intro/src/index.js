dc097360c0d7901b23c8c29aab28b4c39811d964
Thejas <teju270934@gmail.com>
2024-08-20 19:20:12 +0530

    connected the redux to react by instailling the new package called 'react-redux'

--- Cls-Challenges/redux-intro/src/index.js
+++ Cls-Challenges/redux-intro/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import "./index.css";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
 import Store from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
